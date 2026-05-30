b30292c6cb7e8a826d92789ed18d144c5674657d
ecstaticifrit <souptikghosh53@gmail.com>
2026-05-30 17:22:16 +0530

    added company id to /getProfile

--- app/controllers/ProfileController.py
+++ app/controllers/ProfileController.py
@@ -90,6 +90,7 @@ def get_profile(user: dict = Depends(get_current_user)):
             "salary": p.get("salary"),
             "intro": p.get("intro"),
             "photo": p.get("photo"),
+            "companyId": p.get("companyId"),
             "highlights": highlights,
             "tags": tags,
             "sections": sections,
